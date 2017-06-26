411ffaa3a16df33c79fe22c7b7a9b13325326ee9
Francisco José Mañas Álvarez <fma527@inlumine.ual.es>
2017-06-26 13:01:47 +0200

    Preparación del nodo para el control automático
    Permite la conmutación entre el modo de control manual y automático

--- joystick_driving/src/joystick_driving/JoystickDriving.cpp
+++ joystick_driving/src/joystick_driving/JoystickDriving.cpp
@@ -12,6 +12,7 @@ using namespace mrpt;
 using namespace mrpt::utils;
 
 int pwm_steering_const = 0;
+float steer_ref = 0;
 
 bool JoystickDriving::initialize()
 {
@@ -23,18 +24,16 @@ bool JoystickDriving::initialize()
 	m_pub_rev_steering  = m_nh.advertise<std_msgs::Bool>("arduino_daq_GPIO_output4", 10);
 	m_pub_pwm_steering  = m_nh.advertise<std_msgs::UInt8>("arduino_daq_pwm3", 10);
 	m_pub_voltage_pedal = m_nh.advertise<std_msgs::Float64>("arduino_daq_dac0", 10);
-
+	m_pub_contr_status  = m_nh.advertise<std_msgs::Bool>("steer_controller_status", 10);
+	m_pub_steer_ref     = m_nh.advertise<std_msgs::Float64>("steer_controller_ref", 10);
+	m_pub_speed_ref     = m_nh.advertise<std_msgs::Float64>("steer_controller_speed_ref", 10);
 
 	{
 		std_msgs::Bool b;
 		b.data = true;
 		m_pub_rev_relay.publish(b);
-	}
-
-	{	// Pin DIR para pololu
-		std_msgs::Bool msg_b;
-		msg_b.data = true;
-		m_pub_rev_steering.publish(msg_b);
+		m_pub_contr_status.publish(b);
+		m_pub_rev_steering.publish(b);
 	}
 
 	{
@@ -47,22 +46,72 @@ bool JoystickDriving::initialize()
 		std_msgs::Float64 msg_f;
 		msg_f.data = 1.0;
 		m_pub_voltage_pedal.publish(msg_f);
+		m_pub_speed_ref.publish(msg_f);
+		m_pub_steer_ref.publish(msg_f);
 	}
 
 }
 
 bool JoystickDriving::iterate()
 {
-	float x,y,z;
+	float x,y,z,aux_s;
 	int aux;
 	vector<bool> buttons;
-	bool rev;
+	bool rev, control_mode;
+	float Sat = 254;
 
 	bool ok = m_joy.getJoystickPosition(0, x,y,z, buttons);
 	if (!ok) {
 		ROS_ERROR("JoystickDriving: Error reading Joystick!");
 		return false;
 	}
+	const bool mode_btn = (buttons.size()>=7 && !buttons[6]);
+	{
+		std_msgs::Bool b;
+		b.data = mode_btn;
+		control_mode = mode_btn;
+		m_pub_contr_status.publish(b);
+	}
+	if (!control_mode)
+	{	
+		ROS_INFO("Controlador eCAR en modo automatico");
+		// Steer_ref:
+		// ----------------
+		{
+			std_msgs::Float 64 msg_f;
+			//  Aumento de resolucion
+			if (buttons[4]) {
+				aux_s =x * 10;
+			}
+			else {
+				aux_s = x * 40;
+			}
+			//	Saturacion
+			if ((aux_s + steer_ref) < -Sat) {
+				aux_s =   0;
+				steer_ref = -Sat;
+			}
+			if ((aux_s + steer_ref) > Sat) {
+				aux_s = 0;
+				steer_ref = 254;
+			}
+			
+			if (buttons[1]) {
+				steer_ref = steer_ref + aux_s;
+				aux_s = steer_ref;
+			}
+			else {
+				aux_s = steer_ref + aux_s;
+			}
+			ROS_INFO("Steer reference: %f ", aux_s);
+
+			msg_f.data = aux_s;
+			m_pub_steer_ref.publish(msg_f);
+		}
+		return true;
+	} 
+	else
+	{
 
 	// Rev button:
 	// ---------------
@@ -73,21 +122,12 @@ bool JoystickDriving::iterate()
 		m_pub_rev_relay.publish(b);
 	}
 
-	// Rev Steering button:
-	// ---------------
-/*	const bool reverse_steering_btn = (buttons.size()>=3 && !buttons[2]);
-	{
-		std_msgs::Bool msg_b;
-		b_s.data = reverse_steering_btn;
-		m_pub_rev_steering.publish(b_s);
-	}
-*/
 	// PWM steering:
 	// ----------------
 	// [-1,1] -> [-1,1]
 	{
 		std_msgs::UInt8 msg_ui;
-	//  	Aumento de resolución
+	//  	Aumento de resolucion
 		if (buttons[4])
 		{
 			aux = (int)(x * 10);
@@ -159,7 +199,8 @@ bool JoystickDriving::iterate()
 		// NOT IMPLEMENTED YET!!
 	}
 
-  ROS_INFO("Joy: x:%f y:%f z:%f nbut=%u B3=%i", x,y,z, (unsigned int)buttons.size(), buttons[3] ? 1:0 );
+		ROS_INFO("Joy: x:%f y:%f z:%f nbut=%u B3=%i", x,y,z, (unsigned int)buttons.size(), buttons[3] ? 1:0 );
 
 	return true;
+	}
 }
