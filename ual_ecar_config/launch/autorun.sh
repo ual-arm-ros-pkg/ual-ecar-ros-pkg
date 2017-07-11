49c163f416db38b742f22f1488747eb43cb43f96
FranciscoJManasAlvarez <francisco.jose.ma.alv.ual@hotmail.com>
2017-07-11 12:40:49 +0200

    autorun script

--- ual_ecar_config/launch/autorun.sh
+++ ual_ecar_config/launch/autorun.sh
@@ -1,9 +1,9 @@
-#!/bin/bash 
+#!/bin/bash
 
 # Put this file in the user's anacron like:
-# @reboot bash /home/ual/catkin_ws/src/ual-ecar-ros-pkg/ual_ecar_config/launch/autorun.sh
+# @reboot bash /home/ecar/catkin_ws/src/ual-ecar-ros-pkg/ual_ecar_config/launch/autorun.sh
 
 
 source /opt/ros/kinetic/setup.bash
-source /home/ual/catkin_ws/devel/setup.bash
+source /home/ecar/catkin_ws/devel/setup.bash
 roslaunch ual_ecar_config steering_low_level.launch
