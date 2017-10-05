4c352739d0c9eb53d80705be123f73b782c95eb0
Francisco José Mañas Álvarez <fma527@inlumine.ual.es>
2017-10-05 10:42:33 +0200

    Actualización proyecto firmware

--- battery_charge/firmware/battery-firmware/firmware/firmware/batterycharge2pc-structs.h
+++ battery_charge/firmware/battery-firmware/firmware/firmware/batterycharge2pc-structs.h
@@ -62,3 +62,118 @@ enum opcode_t {
 	RESP_WRONG_LEN         = 0xfd,
 	RESP_UNKNOWN_OPCODE    = 0xfe
 };
+
+template <typename Payload>
+struct TBaseFrame
+{
+	const uint8_t  START_FLAG;
+	const uint8_t  OPCODE;
+	const uint8_t  DATALEN;
+	// --------- Payload -------
+	Payload  payload;
+	// -------------------------
+	uint8_t  CHECKSUM;
+	const uint8_t  END_FLAG;
+
+	// Defaults:
+	TBaseFrame(uint8_t opcode) :
+	START_FLAG(FRAME_START_FLAG),
+	OPCODE(opcode),
+	DATALEN(sizeof(Payload)),
+	END_FLAG(FRAME_END_FLAG)
+	{
+	}
+
+	void calc_and_update_checksum()
+	{
+		CHECKSUM = calc_checksum();
+	}
+
+	uint8_t calc_checksum() const
+	{
+		const uint8_t len   = DATALEN; //reinterpret_cast<const uint8_t*>(ptr_frame)[2];
+		const uint8_t *data = reinterpret_cast<const uint8_t*>(&payload);
+		uint8_t ret =0;
+		for (unsigned int i=0;i<len;i++) ret+=*data++;
+		return ret;
+	}
+};
+
+struct TFrameCMD_NOP_payload_t
+{
+};
+struct TFrameCMD_NOP : public TBaseFrame<TFrameCMD_NOP_payload_t>
+{
+	TFrameCMD_NOP() : TBaseFrame(OP_NOP)
+	{
+	}
+};
+
+struct TFrameCMD_OPTO_output_payload_t
+{
+    uint8_t  pin_index;
+    uint8_t  pin_value;
+};
+struct TFrameCMD_OPTO_output : public TBaseFrame<TFrameCMD_OPTO_output_payload_t>
+{
+    // Defaults:
+    TFrameCMD_OPTO_output() : TBaseFrame(OP_SET_OPTO)
+    {
+    }
+};
+
+struct TFrameCMD_OPTO_read_payload_t
+{
+	uint8_t  pin_index;
+};
+struct TFrameCMD_OPTO_read : public TBaseFrame<TFrameCMD_OPTO_read_payload_t>
+{
+	// Defaults:
+	TFrameCMD_OPTO_read() : TBaseFrame(OP_GET_OPTO)
+	{
+	}
+};
+
+struct TFrameCMD_BATTERY_start_payload_t
+{
+	static const uint8_t NUM_BATTERIES = 6;
+
+	uint16_t sampling_period_ms;
+
+	TFrameCMD_BATTERY_start_payload_t() :
+		sampling_period_ms(250)
+
+};
+struct TFrameCMD_BATTERY_start : public TBaseFrame<TFrameCMD_BATTERY_start_payload_t>
+{
+	// Defaults:
+	TFrameCMD_BATTERY_start() : TBaseFrame(OP_START_BAT)
+	{
+	}
+};
+
+struct TFrameCMD_BATTERY_stop_payload_t
+{
+};
+struct TFrameCMD_BATTERY_stop : public TBaseFrame<TFrameCMD_BATTERY_stop_payload_t>
+{
+	// Defaults:
+	TFrameCMD_BATTERY_stop() : TBaseFrame(OP_STOP_BAT)
+	{
+	}
+};
+
+
+struct TFrame_BATTERY_readings_payload_t
+{
+	uint32_t timestamp_ms;
+	int32_t  batteries[6];
+	uint32_t period_ms;
+};
+struct TFrame_BATTERY_readings : public TBaseFrame<TFrame_BATTERY_readings_payload_t>
+{
+	// Defaults:
+	TFrame_BATTERY_readings() : TBaseFrame(RESP_BAT_READINGS)
+	{
+	}
+};
