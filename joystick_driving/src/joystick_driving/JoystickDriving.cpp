f4a8b7cf0d5cf101d2fe0d16d70a60ff415f34f2
Francisco José Mañas Álvarez <fmanas8@alumno.uned.es>
2017-12-21 11:05:56 +0100

    Actualización

--- joystick_driving/src/joystick_driving/JoystickDriving.cpp
+++ joystick_driving/src/joystick_driving/JoystickDriving.cpp
@@ -24,16 +24,19 @@ bool JoystickDriving::initialize()
 	MRPT_TODO("Sub to enable_joystick topic");
 	ROS_INFO("JoystickDriving::initialize() ok.");
 
-	m_pub_rev_relay     = m_nh.advertise<std_msgs::Bool>("arduino_daq_GPIO_output7", 10);
 	m_pub_eje_x			= m_nh.advertise<std_msgs::Float64>("joystick_eje_x", 10);
 	m_pub_eje_y			= m_nh.advertise<std_msgs::Float64>("joystick_eje_y", 10);
-	m_pub_contr_status	= m_nh.advertise<std_msgs::Bool>("vehicle_manual_mode", 10);
+	m_pub_contr_status[0]		= m_nh.advertise<std_msgs::Bool>("vehicle_openloop_mode_steering", 10);
+	m_pub_contr_status[1]		= m_nh.advertise<std_msgs::Bool>("vehicle_openloop_mode_throttle", 10);
+	m_pub_autonomous_driving	= m_nh.advertise<std_msgs::Bool>("vehicle_autonomous_mode", 10);
 
 	{
 		std_msgs::Bool b;
 		b.data = true;
-		m_pub_rev_relay.publish(b);
-		m_pub_contr_status.publish(b);
+		m_pub_contr_status[0].publish(b);
+		m_pub_contr_status[1].publish(b);
+		b.data = false;
+		m_pub_autonomous_driving.publish(b);
 	}
 
 	{
@@ -49,19 +52,29 @@ bool JoystickDriving::iterate()
 {
 	float x,y,z;
 	vector<bool> buttons;
-	bool rev, control_mode;
 
 	bool ok = m_joy.getJoystickPosition(0, x,y,z, buttons);
 	if (!ok) {
 		ROS_ERROR("JoystickDriving: Error reading Joystick!");
 		return false;
 	}
-	const bool mode_btn = (buttons.size()>=7 && !buttons[6]);
+	const bool steer_btn = (buttons.size()>8 && !buttons[8]);
+	{
+		std_msgs::Bool a;
+		a.data = steer_btn;
+		m_pub_contr_status[0].publish(a);
+	}
+	const bool throttle_btn = (buttons.size()>6 && !buttons[6]);
 	{
 		std_msgs::Bool b;
-		b.data = mode_btn;
-		control_mode = mode_btn;
-		m_pub_contr_status.publish(b);
+		b.data = throttle_btn;
+		m_pub_contr_status[1].publish(b);
+	}
+	const bool autonomous_btn = (buttons.size()>7 && !buttons[7]);
+	{
+		std_msgs::Bool c;
+		c.data = mode_btn;
+		m_pub_autonomous_driving.publish(c);
 	}
 
 		// Eje X
@@ -77,13 +90,6 @@ bool JoystickDriving::iterate()
 			aux_s[0] = 1 - eje_x;
 		if (eje_x + aux_s[0] < -1)
 			aux_s[0] = -1 - eje_x;
-		// Cambio del valor central
-		if (buttons[1]) {
-			eje_x = eje_x + aux_s[0];
-			aux_s[0] = eje_x;
-		}
-		else
-			aux_s[0] = eje_x + aux_s[0];
 
 		msg_f.data = aux_s[0];
 		m_pub_eje_x.publish(msg_f);
@@ -100,34 +106,14 @@ bool JoystickDriving::iterate()
 			aux_r[0] = 1 - eje_y;
 		if (eje_y + aux_r[0] < 0)
 			aux_r[0] = - eje_y;
-		// Cambio del valor central
-		if (buttons[0]) {
-			eje_y = eje_y + aux_r[0];
-			aux_r[0] = eje_y;
-		}
-		else
-			aux_r[0] = eje_y + aux_r[0];
 
 		std_msgs::Float64 msg_f;
 		msg_f.data = aux_r[0];
 
 		m_pub_eje_y.publish(msg_f);
 	}
-	// Rev button:
-	// ---------------
-	const bool reverse_btn = (buttons.size()>=4 && !buttons[3]);
-	{
-		std_msgs::Bool b;
-		b.data = reverse_btn;
-		m_pub_rev_relay.publish(b);
-	}
 
-	// ROS_INFO("Joy: x:%f y:%f B3=%i B6=%i", aux_s,aux_r, buttons[3] ? 1:0, buttons[6] ? 1:0);
-	ROS_INFO_COND_NAMED( aux_s[0] !=  aux_s[1] || aux_r[0] !=  aux_r[1] || rev_btn != reverse_btn || mode_btn != control_btn, " test only " ,"Joy: x:%f y:%f B3=%i B6=%i", aux_s[0],aux_r[0], buttons[3] ? 1:0, buttons[6] ? 1:0);
-	aux_s[1] = aux_s[0];
-	aux_r[1] = aux_r[0];
-	rev_btn = reverse_btn;
-	control_btn = mode_btn;
+	ROS_INFO("Joy: x:%f y:%f steer_btn=%i throttle_btn=%i autonomous_btn=%i", aux_s[0],aux_r[0], buttons[6] ? 1:0, buttons[7] ? 1:0, buttons[8] ? 1:0);
 
 	return true;
 }
